aa698efa791dd2ebf954e104860de896715e833a
Connor Pemberton <pemberton.csp@gmail.com>
2022-01-28 03:14:17 -0700

    Refactor Followers in the same way as Following

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowersTask;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowingTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
@@ -50,6 +51,46 @@ public class FollowService {
                 observer.handleFailure(message);
             } else if (msg.getData().containsKey(GetFollowingTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
+                observer.handleException(ex);
+            }
+        }
+    }
+
+    // GET FOLLOWERS
+    public interface GetFollowersObserver {
+        void handleSuccess(List<User> followers, boolean hasMorePages);
+        void handleFailure(String message);
+        void handleException(Exception exception);
+    }
+
+    public void getFollowers(AuthToken currUserAuthToken, User user, int pageSize, User lastFollower, GetFollowersObserver getFollowersObserver) {
+        GetFollowersTask getFollowersTask = new GetFollowersTask(currUserAuthToken,
+                user, pageSize, lastFollower, new GetFollowersHandler(getFollowersObserver));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getFollowersTask);
+    }
+    /**
+     * Message handler (i.e., observer) for GetFollowersTask.
+     */
+    private class GetFollowersHandler extends Handler {
+        private GetFollowersObserver observer;
+
+        public GetFollowersHandler(GetFollowersObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetFollowersTask.SUCCESS_KEY);
+            if (success) {
+                List<User> followers = (List<User>) msg.getData().getSerializable(GetFollowersTask.FOLLOWERS_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFollowersTask.MORE_PAGES_KEY);
+                observer.handleSuccess(followers, hasMorePages);
+            } else if (msg.getData().containsKey(GetFollowersTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFollowersTask.MESSAGE_KEY);
+                observer.handleFailure(message);
+            } else if (msg.getData().containsKey(GetFollowersTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFollowersTask.EXCEPTION_KEY);
                 observer.handleException(ex);
             }
         }
---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FollowersPresenter.java
@@ -0,0 +1,80 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class FollowersPresenter {
+    private static final int PAGE_SIZE = 10;
+
+    public interface View {
+        void displayErrorMessage(String message);
+        void setLoadingStatus(boolean isLoading);
+        void addFollowers(List<User> followers);
+    }
+
+    private View view;
+    private FollowService followService;
+    private User lastFollower;
+    private boolean hasMorePages;
+
+    public boolean hasMorePages() {
+        return hasMorePages;
+    }
+
+    public void setHasMorePages(boolean hasMorePages) {
+        this.hasMorePages = hasMorePages;
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+    }
+
+    private boolean isLoading = false;
+
+    public FollowersPresenter(View view) {
+        this.view = view;
+        followService = new FollowService();
+    }
+
+    public void loadMoreItems(User user) {
+        if (!isLoading()) {   // This guard is important for avoiding a race condition in the scrolling code.
+            isLoading = true;
+            view.setLoadingStatus(isLoading);
+            followService.getFollowers(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastFollower, new GetFollowersObserver());
+        }
+    }
+
+    public class GetFollowersObserver implements FollowService.GetFollowersObserver {
+
+        @Override
+        public void handleSuccess(List<User> followers, boolean hasMorePages) {
+            isLoading = false;
+            view.setLoadingStatus(isLoading);
+
+            lastFollower = (followers.size() > 0) ? followers.get(followers.size() - 1) : null;
+            setHasMorePages(hasMorePages);
+            view.addFollowers(followers);
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            isLoading = false;
+            view.setLoadingStatus(isLoading);
+            view.displayErrorMessage("Failed to get followers: " + message);
+        }
+
+        @Override
+        public void handleException(Exception exception) {
+            isLoading = false;
+            view.setLoadingStatus(isLoading);
+            view.displayErrorMessage("Failed to get followers because of exception: " + exception.getMessage());
+        }
+    }
+}
